f3810d98b55674d76a58ba153a9aa6100d13f322
brzozows <nbrzoza@student.agh.edu.pl>
2025-01-04 00:10:30 +0100

    works

--- src/LedStrip/LedStrip.cpp
+++ src/LedStrip/LedStrip.cpp
@@ -4,59 +4,153 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-
-#define LEDS_NUM 28
+#define LEDS_NUM_1 28
+#define LEDS_NUM_2 60
+#define LEDS_NUM LEDS_NUM_1 + LEDS_NUM_2
+#define TAIL_LEN 26
 #define PIN_NO 7
 
-class LEDStrip17 {
+class LEDStrip17
+{
 public:
     CRGB leds[LEDS_NUM];
-    
-    LEDStrip17() {
+
+    LEDStrip17()
+    {
         // Initialize the LED array
-        FastLED.addLeds<WS2812B, PIN_NO, GRB>(leds, LEDS_NUM);
+        // FastLED.addLeds<WS2812B, PIN_NO, GRB>(leds, LEDS_NUM_GRB);
+        FastLED.addLeds<WS2812B, PIN_NO, GRB>(leds, LEDS_NUM).setCorrection(Candle);
     }
 
-    void begin() {
+    void begin()
+    {
         FastLED.setBrightness(brightness);
         clear(); // Clear the strip on initialization
     }
 
-    void setBrightness(int b) {
+    void setBrightness(int b)
+    {
         brightness = constrain(b, 0, 255);
         FastLED.setBrightness(brightness);
     }
 
-    void clear() {
+    void clear()
+    {
         fill_solid(leds, LEDS_NUM, CRGB::Black);
         FastLED.show();
     }
 
-    void setColor(int index, const CRGB& color) {
-        if (index >= 0 && index < LEDS_NUM) {
+    void setColor(int index, const CRGB &color)
+    {
+        if (index >= 0 && index < LEDS_NUM_1)
+        {
             leds[index] = color;
+        }
+        if (index >= LEDS_NUM_1 && index < LEDS_NUM)
+        {
+            leds[index] = calcColorForLeds2(color);
         }
     }
 
-    void fillColor(const CRGB& color) {
-        fill_solid(leds, LEDS_NUM, color);
+    void fillColor(const CRGB &color)
+    {
+        for (int i = 0; i < LEDS_NUM_1; i++)
+        {
+            leds[i] = color;
+        }
+        for (int i = LEDS_NUM_1; i < LEDS_NUM; i++)
+        {
+            leds[i] = calcColorForLeds2(color);
+        }
+        // fill_solid(leds, LEDS_NUM, color);
     }
 
-    void show() {
+    CRGB calcColorForLeds2(CRGB color)
+    {
+        return CRGB(color.r - 170, color.g, color.b);
+    }
+
+    void show()
+    {
         FastLED.show();
     }
 
-    void oneByOne(const CRGB& color, int delayTime){
-        for(int i=0;i<LEDS_NUM;i++){
-            leds[i] = color;
+    void lightOneAfterOne(const CRGB &color, int delayTime)
+    {
+        for (int i = 0; i < LEDS_NUM; i++)
+        {
+            setColor(i, color);
             show();
             delay(delayTime);
         }
     }
 
+    void lightOneAfterOneTail(const CRGB &color, int delayTime)
+    {
+        for (int i = 0; i < TAIL_LEN; i++)
+        {
+            setColor(i, color);
+            show();
+            delay(delayTime);
+        }
+    }
+
+    void clearOneAfterOne(int delayTime)
+    {
+        for (int i = 0; i < LEDS_NUM; i++)
+        {
+            leds[i] = CRGB::Black;
+            show();
+            delay(delayTime);
+        }
+    }
+
+    void lightOneByOne(const CRGB &color, int delayTime)
+    {
+        for (int i = 0; i < LEDS_NUM; i++)
+        {
+            setColor(i, color);
+            show();
+            delay(delayTime);
+            leds[i] = CRGB::Black;
+        }
+    }
+
+    void blinkAll(const CRGB &color, int delayTime)
+    {
+        fillColor(color);
+        show();
+        delay(delayTime);
+        clear();
+        delay(delayTime);
+    }
+
+    void blinkStar(const CRGB &color, int delayTime)
+    {
+        for (int i = TAIL_LEN; i < LEDS_NUM; i++)
+        {
+            setColor(i, color);
+        }
+        show();
+        delay(delayTime);
+        clear();
+        delay(delayTime);
+    }
+
+    void blinkEveryN(const CRGB &color, int delayTime, int n, int from)
+    {
+        for (int i = from; i < LEDS_NUM; i+=n)
+        {
+            setColor(i, color);
+        }
+        show();
+        delay(delayTime);
+        clear();
+        delay(delayTime);
+    }
+
 private:
     int brightness = 255;
 };
-
 
 #endif // LEDSTRIP_H
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 #include "LedStrip/LedStrip.cpp"
 
 // put function declarations here:
-#define SHOW_DURATION_SEC 3
+#define SEQUENCES_COUNT 5
+#define DEFAULT_COLOR CRGB(255, 90, 0)
 
 uint8_t LIGHT_ON = 0;
 LEDStrip17 led_strip17 = LEDStrip17();
@@ -24,42 +25,71 @@ void loop()
 {
   delay(100);
   int i = 1;
-  uint8_t buf[5];
+  uint8_t buf[1];
   uint8_t buflen = sizeof(buf);
-  unsigned long start = 0;
 
   while (1)
   {
-    delay(10);
-
+    delay(5);
     if (driver.recv(buf, &buflen)) // Non-blocking
     {
       Serial.print("Message: ");
-      char *msg = (char *)buf;
-      Serial.print(msg);
-      if (msg)
+      uint8_t msg = buf[0];
+      Serial.println(msg);
+      if (msg == 1)
       {
-        lightOn(i += 23);
-        start = millis();
+        i = (i + 1) % SEQUENCES_COUNT;
+        lightOn(i);
       }
     }
-    // if (LIGHT_ON && (start + (SHOW_DURATION_SEC * 1000) < millis()))
-    // {
-    //   Serial.println("czyszcze");
-    //   led_strip17.clear();
-    //   LIGHT_ON = 0;
-    // }
   }
 }
 
 // put function definitions here:
-void lightOn(int i)
+void lightOn(int seq)
 {
-  LIGHT_ON = 1;
-  // led_strip17.fillColor(CRGB(i % 256, 100, 100));
-  // led_strip17.show();
-  for(int t=0;t<1;t++){
-    led_strip17.oneByOne(CRGB::AntiqueWhite, 30);
-    led_strip17.clear();
+  if (seq == 1)
+  {
+    for (int t = 0; t < 3; t++)
+    {
+      led_strip17.lightOneAfterOne(DEFAULT_COLOR, 20);
+      led_strip17.clearOneAfterOne(20);
+    }
+  }
+  else if (seq == 2)
+  {
+    for (int t = 0; t < 3; t++)
+    {
+      led_strip17.lightOneByOne(DEFAULT_COLOR, 30);
+    }
+    led_strip17.blinkStar(DEFAULT_COLOR, 300);
+  }
+  else if (seq == 3)
+  {
+    for (int i = 0; i < 5; i++)
+    {
+      led_strip17.blinkAll(DEFAULT_COLOR, 300);
+    }
+  }
+  else if(seq == 4){
+    led_strip17.lightOneAfterOneTail(DEFAULT_COLOR, 60);
+    for (int i = 0; i < 3; i++)
+    {
+      led_strip17.blinkStar(DEFAULT_COLOR, 500);
+    }
+  }
+  else if(seq == 0){
+    for(int i=0;i<2;i++){
+    led_strip17.blinkEveryN(DEFAULT_COLOR, 100, 3, 0);
+    led_strip17.blinkEveryN(DEFAULT_COLOR, 100, 7, 1);
+    led_strip17.blinkEveryN(DEFAULT_COLOR, 100, 5, 2);
+    led_strip17.blinkEveryN(DEFAULT_COLOR, 100, 3, 1);
+    led_strip17.blinkEveryN(DEFAULT_COLOR, 100, 4, 0);
+    led_strip17.blinkEveryN(DEFAULT_COLOR, 100, 7, 5);
+    led_strip17.blinkEveryN(DEFAULT_COLOR, 100, 5, 2);
+    led_strip17.blinkEveryN(DEFAULT_COLOR, 100, 3, 1);
+    led_strip17.blinkEveryN(DEFAULT_COLOR, 100, 6, 2);
+    led_strip17.blinkEveryN(DEFAULT_COLOR, 100, 2, 1);
+    }
   }
 }
